e6f0d13f6aea7ed6d927bc228c10fc8a6bce6f7e
Karabo <78500285+karabomasoeu@users.noreply.github.com>
2026-01-29 21:59:28 +0200

    Update PosNegNumber.py

--- PosNegNumber.py
+++ PosNegNumber.py
@@ -1,2 +1,9 @@
 def PositiveOrNegative():
 number = int(input("Enter any number: "))
+
+if number > 0:
+        print(f"{number} is positive!")
+    elif number < 0:
+        print(f"{number} is negative!")
+    else:
+        print(f"{number} is zero!")
