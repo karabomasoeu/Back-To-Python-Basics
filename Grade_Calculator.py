6d3453d8b37d2d15b287f701d9eb5cba4ef50391
Karabo <78500285+karabomasoeu@users.noreply.github.com>
2026-02-01 23:35:28 +0200

    Update Grade_Calculator.py

--- Grade_Calculator.py
+++ Grade_Calculator.py
@@ -1,1 +1,16 @@
-
+def grade_calculator():
+  
+    score = int(input("Enter your the percentage of your mark: "))
+  
+    if score >= 90:
+        print(f"Your grade is an A")
+    elif score >= 80:
+        print(f"Your grade is a B")
+    elif score >= 70:
+        print(f"Your grade is a C")
+    elif score >= 60:
+        print(f"Your grade is a D")
+    else:
+        print(f"Your grade is an F")
+      
+grade_calculator()
