2c679f5537d5b0bef331d282b5d151ff3f31f85b
Karabo <kbmasoeu@gmail.com>
2026-02-22 00:53:36 +0200

    added function

--- ReverseString.py
+++ ReverseString.py
@@ -1,4 +1,14 @@
-reversed_string = "".join(reversed(input("Enter a word: ")))
+# reversed_string = "".join(reversed(input("Enter a word: ")))
+# print(reversed_string)
 
-print(reversed_string)
+def reverse_string():
+    string = input("Enter a word: ")
+    reversed_string = ""
+
+    for i in range(len(string) -1, -1, -1):
+        reversed_string += string [i]
+    print(reversed_string)
+
+reverse_string()
+
 
