6f69feea34cbc87d67ae624a985e268b7a9c1a10
Karabo <kbmasoeu@gmail.com>
2026-02-23 00:32:56 +0200

    added numbers to a list

---
+++ FindLargestNumber.py
@@ -0,0 +1,10 @@
+def largest_number():
+    number_list = []
+
+    for i in range (5):
+        numbers = int(input("Enter a number: "))
+        number_list.append(numbers)
+    print(number_list)
+
+largest_number()
+
